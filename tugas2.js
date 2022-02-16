da5bbff113946d7615338683122a0f9cfd57fc07
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-16 10:48:13 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,6 @@
+var nama = "Ucup";
+var umur = 50;
+const status perkawinan = false;
+var kepemilikankendaraan = null;
+var jumlahanak = [1, 3, 5, 7];
+console.log(nama + umur + statusperkawinan + kepemilikankendaraan + jumlahanak);
